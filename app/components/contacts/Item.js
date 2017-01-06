a93e46d01063fe7307ae3d47f0309bb8a521bbad
Kirill Mukhov <dissdoc@gmail.com>
2017-01-07 01:56:48 +0300

    init lesson 7

--- app/components/contacts/Item.js
+++ app/components/contacts/Item.js
@@ -2,9 +2,28 @@ import React from 'react';
 
 export default class Item extends React.Component {
 
+	static propTypes = {
+		idx: React.PropTypes.number.isRequired,
+		name: React.PropTypes.any.isRequired
+	};
+
+	constructor() {
+		super();
+
+		this.state = {
+			checked: false
+		};
+	}
+
+	checkElement() {
+		this.setState({
+			checked: !this.state.checked
+		});
+	}
+
 	render() {
 		return (
-			<li key={this.props.idx}>
+			<li key={this.props.idx} onClick={() => this.checkElement()} className={this.state.checked ? 'check' : 'uncheck'}>
 				{this.props.name}
 			</li>
 		);
